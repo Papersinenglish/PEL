28a9ffadca7753df7db9a47bb215ffd2a7114502
Demilade <demiayoola@gmail.com>
2024-05-20 20:49:45 +0100

    commit on 'main'

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,8 +7,8 @@ function Main() {
     return (
 < >
 <div className='d-flex justify-content-center align-items-start'>
-<section id='home' className="py-5 bg d-flex flex-lg-row flex-column">
-<div data-aos="zoom-in"  data-aos-duration="1500" className='px-3 px-md-5 firstl d-flex flex-column'> 
+<section id='home' data-aos="zoom-in"  data-aos-duration="1500" className="py-5 bg d-flex flex-lg-row flex-column">
+<div  className='px-3 px-md-5 firstl d-flex flex-column'> 
 <div className='d-flex gen'>
   <div className='gen-child'><h1 className='header big-text'>PAPERS IN</h1> <h1 className=' header big-text'>ENGLISH AND</h1><h1 className='header big-text'>LINGUISTICS</h1><h1 className='big-text header'>PEL</h1></div>
 </div>
