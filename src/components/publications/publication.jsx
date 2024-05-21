21da3fa8c15cdd3078dbdc4e90fd134f6c4d32f6
Demilade <demiayoola@gmail.com>
2024-05-21 11:33:40 +0100

    link colour

--- src/components/publications/publication.jsx
+++ src/components/publications/publication.jsx
@@ -6,12 +6,11 @@ function Publication() {
 <div className="p-3">
         <section className='pub-header d-flex flex-column justify-content-start'>
         <h3 className="p-2" >Editions</h3>
-            <Link to="/volume24-no-3&4"><div className="p-2"><p><strong>Current Issue -</strong>  Volume 24 - No 3 & 4</p></div></Link>
-            <Link to="/volume24-no-1&2"><p className="p-2">Volume 24 - No 1 & 2 </p></Link>
+            <Link to="/volume24-no-3&4"><div className="pub-header p-2"><p><strong>Current Issue -</strong>  Volume 24 - No 3 & 4</p></div></Link>
+            <Link to="/volume24-no-1&2"><p className="pub-header p-2">Volume 24 - No 1 & 2 </p></Link>
             <hr/>    
         </section>
-        
-    </div>    
+        </div>    
 )
 }
 
