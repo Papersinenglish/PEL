6c9e50523ca0bc30b4ca42b049bbfccf022cf562
Demilade <demiayoola@gmail.com>
2025-04-01 21:12:47 +0100

    new issue

--- src/components/publications/publication.jsx
+++ src/components/publications/publication.jsx
@@ -6,12 +6,13 @@ function Publication() {
 <div className="p-3">
         <section className='pub-header d-flex flex-column justify-content-start'>
         <h3 className="p-2" >Editions</h3>
-            <Link to="/volume25-no-1&2"><div className="pub-header p-2"><p><strong>Current Issue -</strong>  Volume 25 - No 1 & 2</p></div></Link>
+            <Link to="/volume26-no-1&2"><div className="pub-header p-2"><p><strong>Current Issue -</strong>  Volume 26 - No 1 & 2</p></div></Link>
             <Link to="/volume24-no-1&2"><p className="pub-header p-2">Volume 24 - No 1 & 2 </p></Link>
             <Link to="/volume24-no-3&4"><p className="pub-header p-2">Volume 24 - No 3 & 4 </p></Link>
+            <Link to="/volume25-no-1&2"><p className="pub-header p-2">Volume 25 - No 1 & 2 </p></Link>          
             <hr/>    
         </section>
-        </div>    
+        </div>  
 )
 }
 
