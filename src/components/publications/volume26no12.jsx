821b3c48604a135c6cc34e3b04c7f6f79d1cddd0
Demilade <demiayoola@gmail.com>
2025-04-01 21:11:13 +0100

    new issus

---
+++ src/components/publications/volume26no12.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './publication.css'
+
+function Volume25no12() {
+  return (
+    <div className="p-2">
+      <section className='content-header d-flex flex-column justify-content-start'>
+        <h3 className="p-2" >Vol. 25, Nos. 1&2, Mar/Jun 2025</h3>
+        <h5 className="p-2">CONTENTS</h5>
+            <div className="p-2 contents">
+             <a href= '/volume-26-no1&2/AspectsofYorubaPhonologyintheSpeechofIgedeYorubaBilingualResidentsAdeniyiK.pdf'><p> Aspects of Yoruba Phonology in the Speech of Igede-Yoruba Bilingual Migrants in 
+Osun State, Nigeria - 
+ <strong>Kolawole Adeniyi</strong> - 1 </p> </a> 
+              </div>
+              <div className="p-2">
+            <a href= '/volume-26-no1&2/TimingLyricsinSelectedNigerianHipHopMusicAkindeleJAandFabunmiVA.pdf'><p>Timing Lyrics in Selected Nigerian Hip Hop Music and the Implication for 
+Nigerian English Rhythm - 
+<strong> Julianah Ajoke Akindele & Victor Ayomide </strong> - 14 </p></a> 
+              </div>
+              <div className="p-2">
+              <a href= '/volume-26-no1&2/MetaphoricalStudyofAchebeandAdichieJamesAandSongdenRJ.pdf'><p>A Metaphorical Study of Achebe’s Things Fall Apart and Adichie’s Purple Hibiscus. -  
+ <strong>Anthony James & Rejoice James Songden </strong> - 39 </p></a>
+              </div>
+              <div className="p-2">
+              <a href= '/volume-26-no1&2/ReevaluationoftheEnglishFrenchStressPatternsOmageEkwuemeandOlumese.pdf'> <p> A Re-evaluation of the English-French Stress Patterns: A Case for Anglophone Speakers/Learners of French in Nigeria - 
+ <strong> Paschal Dada Omage, Chinyere Uchegbu-Ekwueme & Patience Ebanehita Olumese </strong> - 53 </p></a>
+               </div>
+              <div className="p-2">
+              <a href= '/volume-26-no1&2/MorphophonemicStudyofUmunzePersonalNamesOnahPandPEze.pdf'> <p> A Morpho-phonemic Study of Umunze Personal Names, Southeastern Nigeria - 
+ <strong> Patrick Onah and Patricia Eze </strong> - 64 </p></a>
+               </div>   
+            <hr/>
+
+        </section> 
+    </div>
+  )
+}
+
+export default Volume25no12;
