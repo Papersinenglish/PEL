25e292eb53b53c7c3c0a4111b35018fe50162ccd
Demilade <demiayoola@gmail.com>
2025-10-05 14:07:28 +0100

    new vol

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Volume24no34 from './components/publications/volume24no34';
 import Volume24no12 from './components/publications/volume24no12';
 import Volume25no12 from './components/publications/volume25no12';
 import Volume26no12 from './components/publications/volume26no12';
+import Volume26no34 from './components/publications/volume26no34';
 
 import ScrollToHashElement from "./components/main/ScrolltoHashElement";
 
@@ -36,6 +37,7 @@ function App() {
         <Route path="/volume24-no-3&4" element={<Volume24no34/>} />
         <Route path="/volume25-no-1&2" element={<Volume25no12/>} />
         <Route path="/volume26-no-1&2" element={<Volume26no12/>} />
+        <Route path="/volume26-no-3&4" element={<Volume26no34/>} />
       </Routes>     
 <Footer/> 
     </>
